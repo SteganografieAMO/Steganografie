782f9de8c33350c27ddb3ae0f573806151448a78
andy <andy482382@gmail.com>
2019-10-29 20:24:16 +0100

    *WIP* Decryption

--- Steganografie/DecryptWindow.xaml.cs
+++ Steganografie/DecryptWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
@@ -29,7 +30,15 @@ namespace Steganografie
             InitializeComponent();
 
         }
+        public static string EncodeBase64(string data)
+        {
+            string dummyData = data.Trim().Replace(" ", "+");
+            if (dummyData.Length % 4 > 0)
+                dummyData = dummyData.PadRight(dummyData.Length + 4 - dummyData.Length % 4, '=');
+            byte[] byteArray = Convert.FromBase64String(dummyData);
 
+            return dummyData;
+        }
         public class EncryptDecrypt
         {
             public static string Encrypt(string input, string key)
@@ -46,7 +55,7 @@ namespace Steganografie
             }
             public static string Decrypt(string input, string key)
             {
-                byte[] inputArray = Convert.FromBase64String(input);
+                byte[] inputArray = Encoding.UTF8.GetBytes(input);
                 TripleDESCryptoServiceProvider tripleDES = new TripleDESCryptoServiceProvider();
                 tripleDES.Key = UTF8Encoding.UTF8.GetBytes(key);
                 tripleDES.Mode = CipherMode.ECB;
@@ -55,6 +64,8 @@ namespace Steganografie
                 byte[] resultArray = cTransform.TransformFinalBlock(inputArray, 0, inputArray.Length);
                 tripleDES.Clear();
                 return UTF8Encoding.UTF8.GetString(resultArray);
+
+                
             }
         }
 
@@ -71,11 +82,29 @@ namespace Steganografie
             }
             stringTextBox.IsEnabled = true;
             decryptButton.IsEnabled = true;
+            
+
         }
 
         private void decryptButton_Click(object sender, RoutedEventArgs e)
         {
-            extractText(encryptedImg);
+            var bmp = encryptedImg.Source as BitmapImage;
+
+            int height = bmp.PixelHeight;
+            int width = bmp.PixelWidth;
+            int stride = width * ((bmp.Format.BitsPerPixel + 7) / 8);
+
+            byte[] Imgbytes = new byte[height * stride];
+            bmp.CopyPixels(Imgbytes, stride, 0);
+
+            var bitmap = BitmapImage2Bitmap(bmp);
+
+
+            MessageBox.Show(extractText(bitmap));
+
+            MessageBox.Show(EncryptDecrypt.Decrypt(extractText(bitmap), "sblw-3hn8-sqoy19"));
+
+            
         }
 
         private void encryptWindow_Click(object sender, RoutedEventArgs e)
@@ -148,6 +177,36 @@ namespace Steganografie
             }
 
             return result;
+        }
+        public BitmapImage ConvertBmp(System.Drawing.Image img)
+        {
+            using (var memory = new MemoryStream())
+            {
+                img.Save(memory, ImageFormat.Png);
+                memory.Position = 0;
+
+                var bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memory;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+
+                return bitmapImage;
+            }
+        }
+
+        private Bitmap BitmapImage2Bitmap(BitmapImage bitmapImage)
+        {
+
+            using (MemoryStream outStream = new MemoryStream())
+            {
+                BitmapEncoder enc = new BmpBitmapEncoder();
+                enc.Frames.Add(BitmapFrame.Create(bitmapImage));
+                enc.Save(outStream);
+                System.Drawing.Bitmap bitmap = new System.Drawing.Bitmap(outStream);
+
+                return new Bitmap(bitmap);
+            }
         }
     }
 }
--- Steganografie/MainWindow.xaml.cs
+++ Steganografie/MainWindow.xaml.cs
@@ -230,6 +230,8 @@ namespace Steganografie
             decryp.Show();
             this.Close();
         }
+
+
     }
 }
 
